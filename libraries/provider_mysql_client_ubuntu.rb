b549eb5009e71ed690309950e245bedc97705f54
Peter Walz <pnw@umn.edu>
2015-04-24 14:43:34 -0500

    Quick fix for chef-cookbooks/mysql#228

--- libraries/provider_mysql_client_ubuntu.rb
+++ libraries/provider_mysql_client_ubuntu.rb
@@ -5,7 +5,16 @@ class Chef
     class MysqlClient
       class Ubuntu < Chef::Provider::MysqlClient
         def packages
-          %w(mysql-client-5.5 libmysqlclient-dev)
+          case node['platform_version']
+          when '10.04', '11.04', '11.10'
+            packages = %w(mysql-client-5.1 libmysqlclient-dev)
+          else
+            # This results in us getting v5.5 even on Ubuntu >= 14, where v5.6
+            # is available (in addition to v5.5). If that's a problem,
+            # you should upgrade to v6.x of this cookbook.
+            packages = %w(mysql-client-5.5 libmysqlclient-dev)
+          end
+          packages
         end
       end
     end
